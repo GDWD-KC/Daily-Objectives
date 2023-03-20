ecf32c27977b4761528e6476a7dda225d379ad96
GDWD-KC <casey.tobias@first.edu>
2023-03-20 11:34:48 -0400

    3/20

--- GDWD-105/assets/js/app.js
+++ GDWD-105/assets/js/app.js
@@ -29,7 +29,7 @@ function formatCountdownText(days, hours, minutes, seconds) {
 //Countdown Initialization
 
 //Set Variable for the end of the countdown (deadline)
-let deadline = new Date ("March 13, 2023 23:15:00");
+let deadline = new Date ("April 9, 2023 23:15:00");
 
 // Set Variable to select the h1 that we need
 let countdown = document.getElementById("countdown");
